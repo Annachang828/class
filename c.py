0c4092ebe8af9cc4b822cde089b2cbe36e040f64
Anna Chang <p227738930@hotmail.com>
2021-02-02 10:26:55 +0800

    update class

--- c.py
+++ c.py
@@ -1,21 +1,25 @@
 # class 類別(種類)
 
 class Student:
-    def __init__(self): # intitialize 初始化
-        self.x = 5
+    def __init__(self, name, score): # intitialize 初始化
+        self.name = name
+        self.score = score
         print('我誕生了!')
-        self.do_hw('英文')
-        self.study()
-        self.sleep()
 
     def do_hw(self, hw):
         print('我在做作業', hw)
 
     def study(self):
         print('我在讀書')
+        self.score += 5
 
     def sleep(self):
         print('shh, I am sleeping')
 
-s = Student()
-print(dir(s))
+s1 = Student('Anna', 100)
+s2 = Student('John', 60)
+print(s1.name,s1.score)
+print(s2.name,s2.score)
+
+s2.study()
+print(s2.name, s2.score)
